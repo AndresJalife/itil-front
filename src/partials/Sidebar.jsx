d4e74190a92f5e72566580f284626844db284463
SantiagoTadini <santi.tadini@gmail.com>
2022-05-22 19:41:25 -0300

    Pages por paginas

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -101,7 +101,7 @@ function Sidebar({
           <div>
             <h3 className="text-xs uppercase text-slate-500 font-semibold pl-3">
               <span className="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6" aria-hidden="true">•••</span>
-              <span className="lg:hidden lg:sidebar-expanded:block 2xl:block">Pages</span>
+              <span className="lg:hidden lg:sidebar-expanded:block 2xl:block">Paginas</span>
             </h3>
             <ul className="mt-3">
               {/* Metricas */}
