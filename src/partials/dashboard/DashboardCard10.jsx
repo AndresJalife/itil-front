484ab4896e609bc311bff032f92daee70392c319
SantiagoTadini <santi.tadini@gmail.com>
2022-05-22 19:37:22 -0300

    Menu y una tabla en metricas

--- src/partials/dashboard/DashboardCard10.jsx
+++ src/partials/dashboard/DashboardCard10.jsx
@@ -8,53 +8,33 @@ import Image05 from '../../images/user-36-09.jpg';
 
 function DashboardCard10() {
 
-  const customers = [
+  const items = [
     {
       id: '0',
-      image: Image01,
-      name: 'Alex Shatov',
-      email: 'alexshatov@gmail.com',
-      location: '🇺🇸',
-      spent: '$2,890.66',
+      name: 'Computadora1',
+      tipo: 'Notebook',
     },
     {
       id: '1',
-      image: Image02,
-      name: 'Philip Harbach',
-      email: 'philip.h@gmail.com',
-      location: '🇩🇪',
-      spent: '$2,767.04',
+      name: 'Tablet1',
+      tipo: 'Tablet',
     },
     {
       id: '2',
-      image: Image03,
-      name: 'Mirko Fisuk',
-      email: 'mirkofisuk@gmail.com',
-      location: '🇫🇷',
-      spent: '$2,996.00',
+      name: 'Computadora2',
+      tipo: 'Notebook',
     },
     {
       id: '3',
-      image: Image04,
-      name: 'Olga Semklo',
-      email: 'olga.s@cool.design',
-      location: '🇮🇹',
-      spent: '$1,220.66',
-    },
-    {
-      id: '4',
-      image: Image05,
-      name: 'Burak Long',
-      email: 'longburak@gmail.com',
-      location: '🇬🇧',
-      spent: '$1,890.66',
+      name: 'Impresora1',
+      tipo: 'Impresora',
     },
   ];
 
   return (
     <div className="col-span-full xl:col-span-6 bg-white shadow-lg rounded-sm border border-slate-200">
       <header className="px-5 py-4 border-b border-slate-100">
-        <h2 className="font-semibold text-slate-800">Customers</h2>
+        <h2 className="font-semibold text-slate-800">Configuracion de Hardware</h2>
       </header>
       <div className="p-3">
 
@@ -65,41 +45,30 @@ function DashboardCard10() {
             <thead className="text-xs font-semibold uppercase text-slate-400 bg-slate-50">
               <tr>
                 <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Name</div>
+                  <div className="font-semibold text-left">ID</div>
                 </th>
                 <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Email</div>
+                  <div className="font-semibold text-left">Nombre</div>
                 </th>
                 <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Spent</div>
-                </th>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-center">Country</div>
+                  <div className="font-semibold text-left">Tipo</div>
                 </th>
               </tr>
             </thead>
             {/* Table body */}
             <tbody className="text-sm divide-y divide-slate-100">
               {
-                customers.map(customer => {
+                items.map(item => {
                   return (
-                    <tr key={customer.id}>
+                    <tr key={item.id}>
                       <td className="p-2 whitespace-nowrap">
-                        <div className="flex items-center">
-                          <div className="w-10 h-10 shrink-0 mr-2 sm:mr-3">
-                            <img className="rounded-full" src={customer.image} width="40" height="40" alt={customer.name} />
-                          </div>
-                          <div className="font-medium text-slate-800">{customer.name}</div>
-                        </div>
+                        <div className="text-left">{item.id}</div>
+                      </td>
+                      <td className="p-2 whitespace-nowrap">           
+                        <div className="font-medium text-slate-800">{item.name}</div>
                       </td>
                       <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{customer.email}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left font-medium text-green-500">{customer.spent}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-lg text-center">{customer.location}</div>
+                        <div className="text-left">{item.tipo}</div>
                       </td>
                     </tr>
                   )
