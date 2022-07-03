0fa67161c7ac2cc7c287a81b7bb32f9fc11fee25
Cceecilia <cceciliaroldan@gmail.com>
2022-07-03 04:00:00 -0300

    conecto los items de SLA con el back, su creacion e informacion

--- src/partials/dashboard/DashboardConfSLA.jsx
+++ src/partials/dashboard/DashboardConfSLA.jsx
@@ -1,114 +1,181 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Image01 from '../../images/user-36-05.jpg';
-import Image02 from '../../images/user-36-06.jpg';
-import Image03 from '../../images/user-36-07.jpg';
-import Image04 from '../../images/user-36-08.jpg';
-import Image05 from '../../images/user-36-09.jpg';
+import $ from 'jquery'; 
+
+import LoadingData from './LoadingData';
+import CustomButton from './CustomButton';
+import InfoButton from './InfoButton';
+import { IconButton} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ModalCrearConfSLA from './ModalCrearConfSLA';
+import ModalInfoConfSLA from './ModalInfoConfSLA';
+import ItemVersions from './ItemVersions';
+import {Alert, Button, FormControl, InputLabel, MenuItem, OutlinedInput, Select, Grid} from '@mui/material';
+import ReactTable from 'react-table-6';
+import "react-table-6/react-table.css";
+import useUser from '../useUser';
+import swal from "sweetalert2";
+import { permisosByUserID , customFilter } from '../../utils/Utils';
 
 function DashboardConfSLA() {
 
-  const items = [
-    {
-      id: '0',
-      nombre: 'Almacenamiento en la nube',
-      servicio: 'Almacenamiento',
-      gerente: 'Rodolfo Martinez',
-      fecha_comienzo: '25/02/2021',
-      fecha_fin: '25/02/2023',
-      crucial: 'Si'
-    },
-    {
-      id: '1',
-      nombre: 'Proveedor del servidor',
-      servicio: 'Servidor',
-      gerente: 'Rodolfo Martinez',
-      fecha_comienzo: '01/03/2022',
-      fecha_fin: '01/03/2023',
-      crucial: 'Si'
-    },
-    {
-      id:'2',
-      nombre: 'Almacenamiento en la nube',
-      servicio: 'Almacenamiento',
-      gerente: 'Rodolfo Martinez',
-      fecha_comienzo: '10/04/2022',
-      fecha_fin: '10/10/2022',
-      crucial: 'No'
-    }
-  ];
+  const [items, setItems] = useState(null)
+  const [itemId, setItemId] = useState(null)
+  const [createModalOpen, setCreateModalOpen] = useState(false)
+  const [infoModalState, setInfoModalState] = useState({"open": false, "update": false})
+  const {user, isAdmin, isSupport} = useUser();
 
+  if (!items){
+    $.get("https://itil-back.herokuapp.com/config", function( data, status) {
+      setItems(data)
+    })
+  }
+  
+  const updateDashboard = () => {
+    setItems(null)
+  }
+  
+  const deleteConfigById = (id) => {
+    let url = 'https://itil-back.herokuapp.com/config/' + id.toString();
+
+    swal.fire({
+        title: 'Borrar el item de configuracion',
+        text: "¿Está seguro que desea borrar este item de SLA?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        dangerMode: 'true',
+        confirmButtonText: 'Borrar',
+        cancelButtonText: 'Cancelar'
+    }).then(answer=>{
+        if(answer.isConfirmed){
+            fetch(url, {
+                method: 'DELETE'}).then(() => {
+                swal.fire({
+                    title: "Se borro exitosamente el item de configuracion con id " + id.toString() ,
+                    icon: "success"});
+                setItems(null)
+            }).catch((error) => {console.log(error); swal.fire({
+              title: "Ocurrió un error: ",
+              text: error.message,
+              icon: "error"});});
+        }
+    });
+  }
+    
+  if (items) {
+
+    //return (<ItemVersions></ItemVersions>)
   return (
     <div className="col-span-full xl:col-span-max bg-white shadow-lg rounded-sm border border-slate-200">
       
       <header className="px-5 py-4 border-b border-slate-100" style={{display:'flex', justifyContent:'space-between', cursor:'pointer'}}>
-        <h2 className="font-semibold text-slate-800">Service Level Agreement</h2>
-        <div className="text-sm font-semibold text-white px-1.5 bg-green-500 rounded-full"> + Nuevo </div>  
+        <h2 className="font-semibold text-slate-800">Configuracion de Hardware</h2>
+        <CustomButton  onClick={(e) => { e.stopPropagation(); setCreateModalOpen(true);}}>+ Nuevo </CustomButton>  
+        <ModalCrearConfSLA id="create-confsoft-modal" modalOpen={createModalOpen} setModalOpen={setCreateModalOpen} updateDashboard={updateDashboard} />
+        <ModalInfoConfSLA id="info-software-modal" modalState={infoModalState} setModalState={setInfoModalState} itemID={itemId} updateDashboard={updateDashboard}/>
       </header>
-      <div className="p-3">
+      
+      <div>
+        <ReactTable
+          data={items.filter(item => item.config.versions[0].config_type === 'SLA' )}
+          columns={[
+                {
+                  id: "id",
+                  Header: "ID",
+                  accessor: "config.versions[0].id",
+                  maxWidth: 100,
+                  className: 'text-center'
+                  
+                },
+                {
+                  id: "name",
+                  Header: "Nombre",
+                  accessor: "config.versions[0].name",
+                  minWidth: 300,
+                  className: 'font-medium text-slate-800'
+                },
+                {
+                  id: "service",
+                  Header: "Servicio",
+                  accessor: "config.versions[0].service",
+                  className: 'text-center'
+                },
+                {
+                  id: "service_manager",
+                  Header: "Gerente",
+                  accessor: "config.versions[0].service_manager",
+                  className: 'text-center'
+                },
+                {
+                  id: "start_date",
+                  Header: "Fecha de Inicio",
+                  accessor: "config.versions[0].start_date",
+                  className: 'text-center'
+                },
+                {
+                  id: "end_date",
+                  Header: "Fecha de Fin",
+                  accessor: "config.versions[0].end_date",
+                  className: 'text-center'
+                },
+                
+                {
+                  id: "info",
+                  Header: "Info",
+                  accessor: "config.id",
+                  filterable: false,
+                  sortable: false,
+                  maxWidth: 150,
+                  Cell: ({ value, _ }) =>
+                    (<div className="text-center">
+                  <InfoButton variant="text" onClick={(e) => { e.stopPropagation(); setItemId(value); setInfoModalState({"open": true, "update": true}); }}/>
+                </div>)
+                },
 
-        {/* Table */}
-        <div className="overflow-x-auto">
-          <table className="table-auto w-full">
-            {/* Table header */}
-            <thead className="text-xs font-semibold uppercase text-slate-400 bg-slate-50">
-              <tr>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Nombre</div>
-                </th>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Servicio</div>
-                </th>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Gerente del servicio</div>
-                </th>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Fecha de comienzo</div>
-                </th>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Fecha de finalización</div>
-                </th>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Crucial</div>
-                </th>
-              </tr>
-            </thead>
-            {/* Table body */}
-            <tbody className="text-sm divide-y divide-slate-100">
-              {
-                items.map(item => {
-                  return (
-                    <tr key={item.id}>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{item.nombre}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">           
-                        <div className="font-medium text-slate-800">{item.servicio}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{item.gerente}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{item.fecha_comienzo}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{item.fecha_fin}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{item.crucial}</div>
-                      </td>
-                    </tr>
+                {
+                  id: "borrar",
+                  Header: "Borrar",
+                  accessor: "config.id",
+                  filterable: false,
+                  sortable: false,
+                  maxWidth: 150,
+                  Cell: ({ value, _ }) => ( // { value, columnProps: { rest: { someFunc } } }
+                    <div className={"text-center " + (permisosByUserID(user.sub).configuracion == 2 ? null : "d-none")}>
+                      <IconButton aria-label="delete" onClick = {()=>{deleteConfigById(value)}} color="error">
+                        <DeleteIcon  />
+                      </IconButton>
+                      </div>
                   )
-                })
-              }
-            </tbody>
-          </table>
-
+            },
+          ]}
+          defaultSorted={[
+            {
+              id: "id",
+              desc: true
+            }
+          ]}
+          filterable={true}
+          defaultFiltered={[
+            {
+              //id: "name",
+              //value: "acc"
+            }
+          ]}
+          //onFilteredChange={(filtered) => this.setState({ filtered })}
+          defaultPageSize={10}
+          className="-striped -highlight"
+          />
         </div>
-
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <LoadingData/>
+    )
+  }
 }
+
 
 export default DashboardConfSLA;
---
+++ src/partials/dashboard/ModalCrearConfSLA.jsx
@@ -0,0 +1,182 @@
+import React, { useState, useRef, useEffect } from 'react';
+import Transition from '../../utils/Transition';
+import CustomButton from './CustomButton';
+import "react-datetime/css/react-datetime.css";
+import Datetime from "react-datetime";
+import "./Datetime.css"
+
+import $ from 'jquery'
+
+import swal from 'sweetalert2';
+
+import useUser from '../useUser';
+import {Alert, Button,FormControl, InputLabel, MenuItem, OutlinedInput, Select, Grid} from '@mui/material';
+  
+
+function ModalCrearConfSLA({
+    id,
+    modalOpen,
+    setModalOpen,
+    updateDashboard
+  }) {
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [selectedCrucial, setSelectedCrucial] = useState("");
+
+
+  const handleSubmit =  async (e) => {
+
+    e.preventDefault()
+    const form = new FormData(e.currentTarget);
+
+    let new_config = {
+      "config_type": "SLA",
+      "name": form.get('name'),
+      "service": form.get('service'),
+      "service_manager": form.get('service_manager'),
+      "start_date": startDate,
+      "end_date": endDate,
+      "crucial": selectedCrucial === "Si"
+    };
+
+    console.log(JSON.stringify(new_config))
+    
+    let url = "https://itil-back.herokuapp.com/config"
+    let data = JSON.stringify(new_config)
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': "application/json; charset=utf-8"},
+      body: data,
+    })
+    .then((response) => {
+      if (response.ok){
+        swal.fire({
+          title: "El item de configuración se creo exitosamente",
+          icon: "success"});
+        updateDashboard();
+      } else {
+        console.log(response)
+        swal.fire({
+          title: "Ocurrió un error: ",
+          text: response.statusText,
+          icon: "error"});
+      }})
+    .catch((error) => {console.log(error); swal.fire({
+      title: "Ocurrió un error: ",
+      text: error.message,
+      icon: "error"});});
+    
+    setModalOpen(false);
+      
+  }
+
+  // close if the esc key is pressed
+  useEffect(() => {
+    const keyHandler = ({ keyCode }) => {
+      if (!modalOpen || keyCode !== 27) return;
+      setModalOpen(false);
+    };
+    document.addEventListener('keydown', keyHandler);
+    return () => document.removeEventListener('keydown', keyHandler);
+  });
+
+  useEffect(() => {
+    modalOpen //&& nameInput.current.focus();
+  }, [modalOpen]);
+
+  const handleCrucialChange = (event) => {
+    const { target: {value} } = event;
+    setSelectedCrucial(value);
+  };
+    
+  return (
+    <>
+      {/* Modal backdrop */}
+      <Transition
+        className="fixed inset-0 bg-slate-900 bg-opacity-30 z-50 transition-opacity"
+        show={modalOpen}
+        enter="transition ease-out duration-200"
+        enterStart="opacity-0"
+        enterEnd="opacity-100"
+        leave="transition ease-out duration-100"
+        leaveStart="opacity-100"
+        leaveEnd="opacity-0"
+        aria-hidden="true"
+      />
+      {/* Modal dialog */}
+      <Transition
+        id={id}
+        className="fixed inset-0 z-50 overflow-hidden flex items-start top-20 mb-4 justify-center transform px-4 sm:px-6"
+        role="dialog"
+        aria-modal="true"
+        show={modalOpen}
+        enter="transition ease-in-out duration-200"
+        enterStart="opacity-0 translate-y-4"
+        enterEnd="opacity-100 translate-y-0"
+        leave="transition ease-in-out duration-200"
+        leaveStart="opacity-100 translate-y-0"
+        leaveEnd="opacity-0 translate-y-4"
+      >
+        <div className="bg-white overflow-auto max-w-2xl w-full max-h-full rounded shadow-lg">
+          {/* Search form */}
+          <div className="col-span-full xl:col-span-6 bg-white shadow-lg rounded-sm border border-slate-200">
+
+          <header className="px-5 py-4 border-b border-slate-100 bg-slate-50"> 
+          <h2 className="font-semibold text-slate-800 ">Crear item de configuración de software </h2></header>
+
+          <form onSubmit={(e) => { handleSubmit(e)}}  className="border-b border-slate-200">
+
+
+          {/* <input id={searchId} */}
+              <header className="text-xs uppercase text-slate-400 bg-slate-50 rounded-sm font-semibold p-2">Nombre</header>
+              <label  className="sr-only">Nombre</label>
+              <input name="name" className="w-full border-0 focus:ring-transparent placeholder-slate-400 appearance-none py-3 pl-10 pr-4" type="text" placeholder="Nombre…" />
+              
+              <header className="text-xs uppercase text-slate-400 bg-slate-50 rounded-sm font-semibold p-2">Servicio</header>
+              <label  className="sr-only">Servicio</label>
+              <input name="service" className="w-full border-0 focus:ring-transparent placeholder-slate-400 appearance-none py-3 pl-10 pr-4" type="text" placeholder="Servicio…"  />
+
+              <header className="text-xs uppercase text-slate-400 bg-slate-50 rounded-sm font-semibold p-2">Gerente del servicio</header>
+              <label  className="sr-only">Gerente del servicio</label>
+              <input name="service_manager" className="w-full border-0 focus:ring-transparent placeholder-slate-400 appearance-none py-3 pl-10 pr-4" type="text" placeholder="Gerente…"  />
+                       
+              <header className="text-xs uppercase text-slate-400 bg-slate-50 rounded-sm font-semibold p-2">Crucial</header>
+              <label htmlFor="crucial" className="sr-only">Crucial</label>
+              <Select id="crucial" name="crucial" fullWidth input={<OutlinedInput label="Crucial" />} renderValue={selected => selected} value={selectedCrucial} onChange={e => handleCrucialChange(e)} >
+              {["Si", "No", ""].map(c => <MenuItem key={c} value={c}>{c}</MenuItem>)}
+              </Select>
+                       
+              <Grid container spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                    <header className="text-xs uppercase text-slate-400 bg-slate-50 rounded-sm font-semibold p-2">Fecha de inicio</header>
+                    <label  className="sr-only">Fecha de inicio</label>
+                    <Datetime inputProps={{className:'datetime'}} dateFormat="DD-MM-YYYY" timeFormat={false} value={startDate} onChange={(e) => setStartDate(e)} closeOnSelect='true' className="text-xs uppercase"/>
+                  </Grid>
+                  
+                  <Grid item xs={12} sm={6}>
+                    <header className="text-xs uppercase text-slate-400 bg-slate-50 rounded-sm font-semibold p-2">Fecha de finalizacion</header>
+                    <label  className="sr-only">Fecha de finalizacion</label>
+                    <Datetime inputProps={{className:'datetime'}} dateFormat="DD-MM-YYYY" timeFormat={false} value={endDate} onChange={(e) => setEndDate(e)} closeOnSelect='true' className="text-xs uppercase"/>
+                  </Grid>
+                </Grid>                        
+
+              <header className="text-xs uppercase text-slate-400 bg-slate-50 rounded-sm font-semibold p-2"> </header>
+              <div type="submit" className="bg-slate-50" style={{width:"100%", display:"flex", justifyContent:"space-around", paddingBottom: "10px", paddingTop: "10px"}}>
+                <Button variant="text" onClick={() => setModalOpen(false)}>Cancelar</Button>
+                <CustomButton  type="submit">Crear</CustomButton>
+              </div>
+
+          </form>              
+
+          </div>
+        </div>
+      </Transition>
+    </>
+  );
+}
+  
+export default ModalCrearConfSLA;
+  
