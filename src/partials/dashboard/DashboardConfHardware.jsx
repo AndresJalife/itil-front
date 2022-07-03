7a06ab2325e7aad2107370844abeeec7aac03c2c
Cceecilia <cceciliaroldan@gmail.com>
2022-07-03 02:08:24 -0300

    conecto los items de hardware con el back, su creacion e informacion

--- src/partials/dashboard/DashboardConfHardware.jsx
+++ src/partials/dashboard/DashboardConfHardware.jsx
@@ -4,66 +4,66 @@ import $ from 'jquery';
 
 import LoadingData from './LoadingData';
 import CustomButton from './CustomButton';
-import ModalCrearProblema from './ModalCrearProblema';
+import InfoButton from './InfoButton';
+import { IconButton} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ModalCrearConfHardware from './ModalCrearConfHardware';
+import ModalInfoConfHardware from './ModalInfoConfHardware';
 import ItemVersions from './ItemVersions';
+import {Alert, Button, FormControl, InputLabel, MenuItem, OutlinedInput, Select, Grid} from '@mui/material';
+import ReactTable from 'react-table-6';
+import "react-table-6/react-table.css";
+import useUser from '../useUser';
+import swal from "sweetalert2";
+import { permisosByUserID , customFilter } from '../../utils/Utils';
 
 function DashboardConfHardware() {
 
   const [items, setItems] = useState(null)
-
+  const [itemId, setItemId] = useState(null)
   const [createModalOpen, setCreateModalOpen] = useState(false)
+  const [infoModalState, setInfoModalState] = useState({"open": false, "update": false})
+  const {user, isAdmin, isSupport} = useUser();
 
   if (!items){
-    //hacer get
-    let data=[
-      {
-        id: '0',
-        name: 'Computadora1',
-        tipo: 'Notebook',
-        nro_serie: 45,
-        localizacion: 'Belgrano',
-        proveedor: 'Dell',
-        precio: 10000,
-        fecha_instalacion: '15/6/2016',
-        capacidad: 512,
-      },
-      {
-        id: '1',
-        name: 'Tablet1',
-        tipo: 'Tablet',
-        nro_serie: 765,
-        localizacion: 'San Telmo',
-        proveedor: 'Apple',
-        precio: 20000,
-        fecha_instalacion: '29/3/2019',
-        capacidad: 32,
-      },
-      {
-        id: '2',
-        name: 'Computadora2',
-        tipo: 'Notebook',
-        nro_serie: 1,
-        localizacion: 'La Plata',
-        proveedor: 'Lenovo',
-        precio: 15000,
-        fecha_instalacion: '3/7/2017',
-        capacidad: 32,
-      },
-      {
-        id: '3',
-        name: 'Impresora1',
-        tipo: 'Impresora',
-        nro_serie: 45,
-        localizacion: 'Belgrano',
-        proveedor: 'HP',
-        precio: 20000,
-        fecha_instalacion: '15/11/2016',
-        capacidad: 0,
-      },
-    ];
-    setItems(data)
+    $.get("https://itil-back.herokuapp.com/config", function( data, status) {
+      setItems(data)
+    })
   }
+  
+  const updateDashboard = () => {
+    setItems(null)
+  }
+  
+  const deleteConfigById = (id) => {
+    let url = 'https://itil-back.herokuapp.com/config/' + id.toString();
 
+    swal.fire({
+        title: 'Borrar el item de configuracion',
+        text: "¿Está seguro que desea borrar este item de hardware?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        dangerMode: 'true',
+        confirmButtonText: 'Borrar',
+        cancelButtonText: 'Cancelar'
+    }).then(answer=>{
+        if(answer.isConfirmed){
+            fetch(url, {
+                method: 'DELETE'}).then(() => {
+                swal.fire({
+                    title: "Se borro exitosamente el item de configuracion con id " + id.toString() ,
+                    icon: "success"});
+                setItems(null)
+            }).catch((error) => {console.log(error); swal.fire({
+              title: "Ocurrió un error: ",
+              text: error.message,
+              icon: "error"});});
+        }
+    });
+  }
+    
   if (items) {
 
     //return (<ItemVersions></ItemVersions>)
@@ -73,94 +73,109 @@ function DashboardConfHardware() {
       <header className="px-5 py-4 border-b border-slate-100" style={{display:'flex', justifyContent:'space-between', cursor:'pointer'}}>
         <h2 className="font-semibold text-slate-800">Configuracion de Hardware</h2>
         <CustomButton  onClick={(e) => { e.stopPropagation(); setCreateModalOpen(true);}}>+ Nuevo </CustomButton>  
-        <ModalCrearProblema id="create-confhard-modal" modalOpen={createModalOpen} setModalOpen={setCreateModalOpen} />
-
+        <ModalCrearConfHardware id="create-confhard-modal" modalOpen={createModalOpen} setModalOpen={setCreateModalOpen} updateDashboard={updateDashboard} />
+        <ModalInfoConfHardware id="info-hardware-modal" modalState={infoModalState} setModalState={setInfoModalState} itemID={itemId} updateDashboard={updateDashboard}/>
       </header>
-      <div className="p-3">
+      
+      <div>
+        <ReactTable
+          data={items.filter(item => item.config.versions[0].config_type === 'hardware' )}
+          columns={[
+                {
+                  id: "id",
+                  Header: "ID",
+                  accessor: "config.versions[0].id",
+                  maxWidth: 100,
+                  className: 'text-center'
+                  
+                },
+                {
+                  id: "name",
+                  Header: "Nombre",
+                  accessor: "config.versions[0].name",
+                  minWidth: 300,
+                  className: 'font-medium text-slate-800'
+                },
+                {
+                  id: "type",
+                  Header: "Tipo",
+                  accessor: "config.versions[0].type",
+                  className: 'text-center'
+                },
+                {
+                  id: "location",
+                  Header: "Localizacion",
+                  accessor: "config.versions[0].location",
+                  className: 'text-center'
+                },
+                {
+                  id: "provider",
+                  Header: "Proveedor",
+                  accessor: "config.versions[0].provider",
+                  className: 'text-center'
+                },
+                {
+                  id: "capacity",
+                  Header: "Capacidad",
+                  accessor: "config.versions[0].capacity",
+                  className: 'text-center'
+                },
+                
+                {
+                  id: "info",
+                  Header: "Info",
+                  accessor: "config.id",
+                  filterable: false,
+                  sortable: false,
+                  maxWidth: 150,
+                  Cell: ({ value, _ }) =>
+                    (<div className="text-center">
+                  <InfoButton variant="text" onClick={(e) => { e.stopPropagation(); setItemId(value); setInfoModalState({"open": true, "update": true}); }}/>
+                </div>)
+                },
 
-        {/* Table */}
-        <div className="overflow-x-auto">
-          <table className="table-auto w-full">
-            {/* Table header */}
-            <thead className="text-xs font-semibold uppercase text-slate-400 bg-slate-50">
-              <tr>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">ID</div>
-                </th>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Nombre</div>
-                </th>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Tipo</div>
-                </th>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Nro Serie</div>
-                </th>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Localizacion</div>
-                </th>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Proveedor</div>
-                </th>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Precio</div>
-                </th>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Fecha de instalacion</div>
-                </th>
-                <th className="p-2 whitespace-nowrap">
-                  <div className="font-semibold text-left">Capacidad</div>
-                </th>
-              </tr>
-            </thead>
-            {/* Table body */}
-            <tbody className="text-sm divide-y divide-slate-100">
-              {
-                items.map(item => {
-                  return (
-                    <tr key={item.id}>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{item.id}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">           
-                        <div className="font-medium text-slate-800">{item.name}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{item.tipo}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{item.nro_serie}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{item.localizacion}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{item.proveedor}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{item.precio}$</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{item.fecha_instalacion}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{item.capacidad} GB</div>
-                      </td>
-                    </tr>
+                {
+                  id: "borrar",
+                  Header: "Borrar",
+                  accessor: "config.id",
+                  filterable: false,
+                  sortable: false,
+                  maxWidth: 150,
+                  Cell: ({ value, _ }) => ( // { value, columnProps: { rest: { someFunc } } }
+                    <div className={"text-center " + (permisosByUserID(user.sub).configuracion == 2 ? null : "d-none")}>
+                      <IconButton aria-label="delete" onClick = {()=>{deleteConfigById(value)}} color="error">
+                        <DeleteIcon  />
+                      </IconButton>
+                      </div>
                   )
-                })
-              }
-            </tbody>
-          </table>
-
-        </div>
-
-      </div>
-    </div>
-  );
-            } else {
-              return ( <LoadingData/> )
+            },
+          ]}
+          defaultSorted={[
+            {
+              id: "id",
+              desc: true
             }
+          ]}
+          filterable={true}
+          defaultFiltered={[
+            {
+              //id: "name",
+              //value: "acc"
+            }
+          ]}
+          //onFilteredChange={(filtered) => this.setState({ filtered })}
+          defaultPageSize={10}
+          className="-striped -highlight"
+          />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <LoadingData/>
+    )
+  }
 }
+
 
 export default DashboardConfHardware;
